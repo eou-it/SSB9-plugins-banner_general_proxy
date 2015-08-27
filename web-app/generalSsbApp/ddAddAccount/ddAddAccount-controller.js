53b425befbbe03526e26dbf9f0619d341d8e5205
Daren_Dunn <daren.dunn@ellucian.com>
2015-08-27 10:28:01 -0400

    added in skeleton for add account ui pieces; update common plugin with add account changes

---
+++ web-app/generalSsbApp/ddAddAccount/ddAddAccount-controller.js
@@ -0,0 +1,6 @@
+/*******************************************************************************
+ Copyright 2015 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+generalSsbAppControllers.controller('ddAddAccountController',['$scope', function ($scope){
+
+}]);
