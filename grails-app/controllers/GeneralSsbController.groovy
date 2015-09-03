8c006636fea56cdd5995de2a8e7bd2910fdc277c
Daren_Dunn <daren.dunn@ellucian.com>
2015-09-03 09:19:24 -0400

    pulling test code out of GeneralSsbController.groovy

--- grails-app/controllers/GeneralSsbController.groovy
+++ grails-app/controllers/GeneralSsbController.groovy
@@ -6,22 +6,13 @@
 import grails.converters.JSON
 import net.hedtech.banner.exceptions.ApplicationException
 import net.hedtech.banner.i18n.LocalizeUtil
-
 import org.apache.log4j.Logger
 import org.codehaus.groovy.grails.plugins.web.taglib.ValidationTagLib
-
-/* DJD imports for my local testing */
-
-import net.hedtech.banner.general.overall.DirectDepositAccount
-import net.hedtech.banner.general.overall.DirectDepositAccountService
-import net.hedtech.banner.security.BannerGrantedAuthorityService
-
 
 class GeneralSsbController  {
 
     def log = Logger.getLogger( this.getClass() )
     static defaultAction = "landingPage"
-    def directDepositAccountService
 
 
     def fetchDate() {
@@ -58,96 +49,5 @@ class GeneralSsbController  {
         }
     }
 
-    /* DJD action for my local testing */
-    def dirdAccounts() {
-        def model = [:]
-        def pidm = BannerGrantedAuthorityService.getPidm()
-        
-        log.warn "Employee pidm = $pidm"
-        
-        model = DirectDepositAccount.fetchByPidm(pidm)
-        //model = DirectDepositAccount.findByPidm(pidm)
-        
-        log.warn "Employee model = $model.size()"
-
-        render model
-    }
-    
-    /* DJD action for my local testing */
-    def newAcct() {
-        def model = [:]
-        def pidm = BannerGrantedAuthorityService.getPidm()
-        
-        def acct = new DirectDepositAccount(
-            pidm: pidm,
-            status: "A",
-            documentType: "D",
-            priority: 16,
-            apIndicator: "A",
-            hrIndicator: "I",
-//          lastModified: $lastModified,
-//          lastModifiedBy: $lastModifiedBy,
-            bankAccountNum: 36958575,
-            bankRoutingNum: 123478902,
-            amount: null,
-            percent: 100.0,
-            accountType: "C",
-//          addressTypeCode: $addressTypeCode,
-//          addressSequenceNum: $addressSequenceNum,
-            intlAchTransactionIndicator: "N"
-//          isoCode: $isoCode,
-//          apAchTransactionTypeCode: $apAchTransactionTypeCode
-//          iatAddressTypeCode: $iatAddressTypeCode
-//          iatAddessSequenceNum: $iatAddessSequenceNum
-        )
-        
-        //def newMap = [domainModel: acct]
-
-        directDepositAccountService.create([domainModel: acct], true)
-        //DirectDepositAccountService.create(acct)
-        //acct.save()
-
-        model = DirectDepositAccount.fetchByPidm(pidm)
-
-        render model
-    }
-    
-    /* DJD action for my local testing */
-    def delAcct() {
-        def pidm = BannerGrantedAuthorityService.getPidm()
-        def model = [:]
-        def acct = DirectDepositAccount.get(160);
-        
-        log.warn "Employee acct = $acct"
-        
-        //good - directDepositAccountService.delete(acct)
-        
-        model = DirectDepositAccount.fetchByPidm(pidm)
-        
-        render model
-    }
-    
-    /* DJD action for my local testing */
-    def apAccts() {
-        def model = [:]
-        def pidm = BannerGrantedAuthorityService.getPidm()
-        
-        model = DirectDepositAccount.fetchByPidmAndApIndicator(pidm)
-
-        render model
-    }
-    
-    /* DJD action for my local testing */
-    def fetchAcct() {
-        def model = [:]
-        def pidm = BannerGrantedAuthorityService.getPidm()
-        def bankRoutingNum = "123478902"
-        def bankAccountNum = "29149281945"
-        def accountType = "S"
-        
-        model = DirectDepositAccount.fetchByPidmAndAccountInfo(pidm, bankRoutingNum, bankAccountNum, accountType)
-
-        render model
-    }
 
 }
