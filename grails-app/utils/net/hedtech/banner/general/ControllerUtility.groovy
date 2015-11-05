e7d5d48b97941cf93ad3702b4da3ab6e84f704df
Jim Caley <jim.caley@ellucian.com>
2015-11-05 14:46:35 -0500

    Format currency for payroll allocations.

--- grails-app/utils/net/hedtech/banner/general/ControllerUtility.groovy
+++ grails-app/utils/net/hedtech/banner/general/ControllerUtility.groovy
@@ -1,7 +1,9 @@
 package net.hedtech.banner.general
 
+import net.hedtech.banner.general.system.InstitutionalDescription
 import org.apache.log4j.Logger
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.context.request.RequestContextHolder
 
 class ControllerUtility {
 
@@ -15,6 +17,21 @@ class ControllerUtility {
             log.error(it)
             throw it
         }
+    }
+
+
+    public static getCurrencyCode() {
+        def currencyCode
+        def session = RequestContextHolder?.currentRequestAttributes()?.request?.session
+
+        if (session?.getAttribute("baseCurrencyCode")) {
+            currencyCode = session.getAttribute("baseCurrencyCode")
+        } else {
+            currencyCode = InstitutionalDescription.fetchByKey()?.baseCurrCode
+            session.setAttribute("baseCurrencyCode", currencyCode)
+        }
+
+        return currencyCode
     }
 
 }
