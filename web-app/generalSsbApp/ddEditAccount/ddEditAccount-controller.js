63f4bba8beb6d23530a256fcc6bb9195d6622428
Daren_Dunn <daren.dunn@ellucian.com>
2015-11-06 15:55:34 -0500

    DID-250 - Format Account and Routing Number input for proper IBAN validation I18n the Amount label on editAccount page

--- web-app/generalSsbApp/ddEditAccount/ddEditAccount-controller.js
+++ web-app/generalSsbApp/ddEditAccount/ddEditAccount-controller.js
@@ -11,44 +11,67 @@ generalSsbAppControllers.controller('ddEditAccountController', ['$scope', '$moda
 
     $scope.editAccountService = ddEditAccountService;
 
+    //routing and account number should only contain upper case letters and digits
+    var invalidCharRegEx = /[^A-Za-z0-9]/i;
     $scope.routingNumErr = false;
     $scope.routingNumMessage;
 
     $scope.validateRoutingNum = function () {
         if($scope.account.bankRoutingInfo.bankRoutingNum){
-            ddEditAccountService.getBankInfo($scope.account.bankRoutingInfo.bankRoutingNum).$promise.then(function (response) {
-                if(response.failure) {
-                    $scope.routingNumErr = true;
-                    $scope.routingNumMessage = $filter('i18n')('directDeposit.invalid.routing.number');
-                    $scope.account.bankRoutingInfo.bankName = null;
-                    notificationCenterService.displayNotifications($scope.routingNumMessage, "error");
-                }
-                else{
-                    $scope.routingNumMessage = response.bankName;
-                    $scope.routingNumErr = false;
-                    notificationCenterService.clearNotifications();
-                }
-            });
+
+            if( invalidCharRegEx.test($scope.account.bankRoutingInfo.bankRoutingNum) ){
+                $scope.routingNumErr = true;
+                $scope.routingNumMessage = $filter('i18n')('directDeposit.invalid.routing.number');
+                $scope.account.bankRoutingInfo.bankName = null;
+                notificationCenterService.displayNotifications($scope.routingNumMessage, "error");
+            }
+            else {
+                $scope.account.bankRoutingInfo.bankRoutingNum = $scope.account.bankRoutingInfo.bankRoutingNum.toUpperCase();
+
+                ddEditAccountService.getBankInfo($scope.account.bankRoutingInfo.bankRoutingNum).$promise.then(function (response) {
+                    if(response.failure) {
+                        $scope.routingNumErr = true;
+                        $scope.routingNumMessage = $filter('i18n')('directDeposit.invalid.routing.number');
+                        $scope.account.bankRoutingInfo.bankName = null;
+                        notificationCenterService.displayNotifications($scope.routingNumMessage, "error");
+                    }
+                    else {
+                        $scope.routingNumMessage = response.bankName;
+                        $scope.routingNumErr = false;
+                        notificationCenterService.clearNotifications();
+                    }
+                });
+            }
         }
     };
-    
+
     $scope.accountNumErr = false;
     $scope.accountNumMessage;
     
     $scope.validateAccountNum = function () {
         if($scope.account.bankAccountNum){
-            ddEditAccountService.validateAccountNum($scope.account.bankAccountNum).$promise.then(function (response) {
-                if(response.failure) {
-                    $scope.accountNumErr = true;
-                    $scope.accountNumMessage = $filter('i18n')('directDeposit.invalid.account.number');
-                    notificationCenterService.displayNotifications($scope.accountNumMessage, "error");
-                }
-                else{
-                    $scope.accountNumMessage = null;
-                    $scope.accountNumErr = false;
-                    notificationCenterService.clearNotifications();
-                }
-            });
+
+            if( invalidCharRegEx.test($scope.account.bankAccountNum) ){
+                $scope.accountNumErr = true;
+                $scope.accountNumMessage = $filter('i18n')('directDeposit.invalid.account.number');
+                notificationCenterService.displayNotifications($scope.accountNumMessage, "error");
+            }
+            else {
+                $scope.account.bankAccountNum = $scope.account.bankAccountNum.toUpperCase();
+
+                ddEditAccountService.validateAccountNum($scope.account.bankAccountNum).$promise.then(function (response) {
+                    if(response.failure) {
+                        $scope.accountNumErr = true;
+                        $scope.accountNumMessage = $filter('i18n')('directDeposit.invalid.account.number');
+                        notificationCenterService.displayNotifications($scope.accountNumMessage, "error");
+                    }
+                    else {
+                        $scope.accountNumMessage = null;
+                        $scope.accountNumErr = false;
+                        notificationCenterService.clearNotifications();
+                    }
+                });
+            }
         }
     };
     
