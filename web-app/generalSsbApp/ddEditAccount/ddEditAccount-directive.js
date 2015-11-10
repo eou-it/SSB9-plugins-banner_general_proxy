b95effaf55c584277de18730a92c6ae708700f0f
Daren_Dunn <daren.dunn@ellucian.com>
2015-11-10 12:19:49 -0500

    add directive to close dropdownmenu when tab key pressed

--- web-app/generalSsbApp/ddEditAccount/ddEditAccount-directive.js
+++ web-app/generalSsbApp/ddEditAccount/ddEditAccount-directive.js
@@ -23,3 +23,39 @@ generalSsbAppDirectives.directive('editModalSaveButton',[function () {
         template: "{{(creatingNewAccount === true ? 'directDeposit.button.save.new' : 'directDeposit.button.save.changes')|i18n}}"
     };
 }]);
+
+/* 
+ * usage:
+ * place dropdown-helper="begin" on the element with the data-toggle attribute so that when user keys to the previous focusable element
+ * with shift+tab the dropdown menu is closed. Place dropdown-helper="end" on the last item in the menu so when the user keys to the
+ * next focusable element with tab the dropdown menu closes.
+ */
+generalSsbAppDirectives.directive('dropdownHelper', [function () {
+    return {
+        restrict: 'A',
+        link: function (scope, elem, attrs) {
+
+            elem.bind('keydown', function(event) {
+                var code = event.keyCode || event.which;
+
+                if (code === 9) {
+                    if(attrs.dropdownHelper === 'begin'){
+                        if(event.shiftKey){
+                            //close dropdown if it is open when shift+tab off element
+                        	if(elem.parent().hasClass('open')){
+                        		elem.dropdown('toggle');
+                        	}
+                        }
+                    }
+                    else if(attrs.dropdownHelper === 'end'){
+                        if(!event.shiftKey){
+                            //close dropdown when tab off element
+                            elem.dropdown('toggle');
+                        }
+                    }
+                }
+            });
+        }
+    };
+}]);
+
