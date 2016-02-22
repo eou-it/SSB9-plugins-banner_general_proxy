514f941068016c7bf7760b6ab32ba9503ac38b6f
Daren_Dunn <daren.dunn@ellucian.com>
2016-02-22 10:31:53 -0500

    modify disclaimer id's to meet ARIA standards

--- web-app/generalSsbApp/ddEditAccount/ddEditAccount-directive.js
+++ web-app/generalSsbApp/ddEditAccount/ddEditAccount-directive.js
@@ -213,6 +213,11 @@ generalSsbAppDirectives.directive('maskInput', ['$filter', function ($filter) {
 generalSsbAppDirectives.directive('modalDisclaimer', [function () {
     return {
         restrict: 'E',
-        templateUrl: '../generalSsbApp/ddEditAccount/modalDisclaimer.html'
+        templateUrl: '../generalSsbApp/ddEditAccount/modalDisclaimer.html',
+        link: function (scope, elem, attrs){
+            scope.disclaimerId = attrs.id;
+
+            elem.removeAttr('id');
+        }
     };
 }]);
