90fb3c94008adae066c24eb7239754fb0eeb459f
Daren_Dunn <daren.dunn@ellucian.com>
2016-02-29 09:57:33 -0500

    add request to updateAccountController tests and merge master into seeddata

--- test/integration/net/hedtech/banner/general/UpdateAccountControllerTests.groovy
+++ test/integration/net/hedtech/banner/general/UpdateAccountControllerTests.groovy
@@ -37,6 +37,24 @@ class UpdateAccountControllerTests extends BaseIntegrationTestCase {
         loginSSB 'MYE000001', '111111'
         
         controller.request.contentType = "text/json"
+        controller.request.json = '''{
+            pidm:null,
+            status:null,
+            apIndicator:"I",
+            hrIndicator:"A",
+            bankAccountNum:"0822051515",
+            amount:null,
+            percent:100,
+            accountType:"C",
+            bankRoutingInfo:{
+                bankName:"First Fidelity",
+                bankRoutingNum:"123478902",
+            },
+            amountType:"amount",
+            priority:"2",
+            newPosition:"2"
+        }'''
+
         controller.createAccount()
         def dataForNullCheck = controller.response.contentAsString
         def data = JSON.parse( dataForNullCheck )
@@ -48,6 +66,39 @@ class UpdateAccountControllerTests extends BaseIntegrationTestCase {
         loginSSB 'MYE000001', '111111'
         
         controller.request.contentType = "text/json"
+        controller.request.json = '''[{
+            class:"net.hedtech.banner.general.overall.DirectDepositAccount",
+            accountType:"C",
+            addressSequenceNum:1,
+            addressTypeCode:"PR",
+            amount:null,
+            apAchTransactionTypeCode:null,
+            apIndicator:"A",
+            bankAccountNum:"9876543",
+            bankRoutingInfo:{
+                class:"net.hedtech.banner.general.crossproduct.BankRoutingInfo",
+                bankName:"First National Bank",
+                bankRoutingNum:"234798944",
+                dataOrigin:null,
+                lastModified:"1999-08-17T03:34:22Z",
+                lastModifiedBy:"PAYROLL"
+            },
+            dataOrigin:"Banner",
+            documentType:"D",
+            hrIndicator:"I",
+            iatAddessSequenceNum:null,
+            iatAddressTypeCode:null,
+            intlAchTransactionIndicator:"N",
+            isoCode:null,
+            lastModified:"2016-02-26T19:48:41Z",
+            lastModifiedBy:"mye000001",
+            percent:100,
+            pidm:36732,
+            priority:3,
+            status:"P",
+            apDelete:true
+        }]'''
+
         controller.deleteAccounts()
         def dataForNullCheck = controller.response.contentAsString
         def data = JSON.parse( dataForNullCheck )
