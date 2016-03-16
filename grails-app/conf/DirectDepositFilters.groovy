3e63ede50689b16c6a0421255ef108e23009bfcc
Jim Caley <jim.caley@ellucian.com>
2016-03-16 15:47:23 -0400

    Make DD filter handle parameter arrays in addition to maps. Add error logging if an unknown parameter type is found.

--- grails-app/conf/DirectDepositFilters.groovy
+++ grails-app/conf/DirectDepositFilters.groovy
@@ -1,12 +1,23 @@
 import net.hedtech.banner.general.DirectDepositUtility
+import org.apache.log4j.Logger
 
 class DirectDepositFilters {
+    private final log = Logger.getLogger(DirectDepositFilters.class)
+
     def filters = {
-        // Sanitize all parameter values for all requests in the Direct Deposit application.
-        sanitizeFilter(controller:'*', action:'*') {
+        // Sanitize all parameter values for all actions in the UpdateAccount controller.
+        sanitizeFilter(controller:'updateAccount', action:'*') {
             before = {
-                def map = request?.JSON ?: params
-                DirectDepositUtility.sanitizeMap(map)
+                def requestParams = request?.JSON ?: params
+
+                if (requestParams in List) {
+                    requestParams.each {item -> DirectDepositUtility.sanitizeMap(item)}
+                } else if (requestParams in Map) {
+                    DirectDepositUtility.sanitizeMap(requestParams)
+                } else {
+                    log.error(new Exception('Unknown request parameter type. Expected Map or Array.'))
+                    return false
+                }
             }
         }
     }
