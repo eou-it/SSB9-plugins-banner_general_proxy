f31100a7087ea2f73959954e2f3200004891a380
Craig Riddle <craig.riddle@ellucian.com>
2017-10-03 19:04:06 +0100

    put back restApiAuth bean

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,11 +1,26 @@
 /*******************************************************************************
  Copyright 2017 Ellucian Company L.P. and its affiliates.
- ****************************************************************************** */
+ *******************************************************************************/
+import net.hedtech.api.security.RestApiAccessDeniedHandler
+import net.hedtech.api.security.RestApiAuthenticationEntryPoint
+import net.hedtech.banner.PbBannerRestfulServiceAdapter
 
 // Place your Spring DSL code here
 import net.hedtech.banner.converters.JobsubOutputMarshaller
 
 beans = {
+    //bean being used by rest api, adapter PageBuilder to Banner
+    restfulServiceAdapter(PbBannerRestfulServiceAdapter)
+
+    //Banner equivalent of bean above
+    //bannerRestfulApiServiceBaseAdapter(RestfulApiServiceBaseAdapter)
+
+    restApiAuthenticationEntryPoint(RestApiAuthenticationEntryPoint) {
+        realmName = 'Banner REST API Realm'
+    }
+
+    restApiAccessDeniedHandler(RestApiAccessDeniedHandler)
+
 
     jobsubOutputMarshaller(JobsubOutputMarshaller) { bean ->
         bean.autowire = 'byName'
