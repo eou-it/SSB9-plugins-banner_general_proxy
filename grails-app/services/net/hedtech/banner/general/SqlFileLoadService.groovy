a5535421718a5d6a08b6b3c2768c95df9684da4c
Daren_Dunn <daren.dunn@ellucian.com>
2018-05-25 12:13:44 -0400

    rename resources folder to prevent shadowing of ApplicationResources

--- grails-app/services/net/hedtech/banner/general/SqlFileLoadService.groovy
+++ grails-app/services/net/hedtech/banner/general/SqlFileLoadService.groovy
@@ -23,8 +23,8 @@ class SqlFileLoadService {
     Map getSqlTextMap() {
         String fileLocation
         if (!grailsApplication.config?.proxySqlLoad?.file) {
-            log.debug('no config, using resource: /resources/proxySqlLoad.txt')
-            fileLocation = getClass().getResource('/resources/proxySqlLoad.txt').getFile()
+            log.debug('no config, using resource: /resrc/proxySqlLoad.txt')
+            fileLocation = getClass().getResource('/resrc/proxySqlLoad.txt').getFile()
         }
         else {
             fileLocation = grailsApplication.config?.proxySqlLoad?.file
