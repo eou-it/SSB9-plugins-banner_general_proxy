7bf50c25bafd311965300ea067a4d2f416652913
Daren_Dunn <daren.dunn@ellucian.com>
2018-08-30 08:42:52 -0400

    BXEGS-1250 add web-app

---
+++ web-app/proxyApp/finaid/proxyViewAwardPackage-controller.js
@@ -0,0 +1,41 @@
+/********************************************************************************
+ Copyright 2018 Ellucian Company L.P. and its affiliates.
+ ********************************************************************************/
+proxyAppControllers.controller('proxyAwardPackage',['$scope','$rootScope','$stateParams', 'proxyAppService', '$filter',
+    function ($scope, $rootScope, $stateParams, proxyAppService, $filter) {
+
+        $scope.pidm = $stateParams.pidm;
+        $scope.aidYear = '';
+        $scope.awardPackage = {test: 'bonkers'};
+
+        $scope.submit = function() {
+            proxyAppService.getAwardPackage({aidYear: $scope.aidYear, pidm: $scope.pidm}).$promise.then(function (response) {
+                $scope.awardPackage = response;
+            });
+        };
+
+        $scope.getStatusTextNonPell = function(option) {
+            var text = '';
+            switch (option) {
+                case 1:
+                    text = 'Full-Time';
+                    break;
+                case 2:
+                    text = '3/4 Time';
+                    break;
+                case 3:
+                    text = '1/2 Time';
+                    break;
+                case 4:
+                    text = 'Less than 1/2 Time';
+                    break;
+            }
+
+            return text;
+        };
+
+        $scope.now = function() {
+            return moment().format('YYYY-MM-DD');
+        };
+    }
+]);
