9f3b8d6806aa67fc31b65fd171879bbd049dfe33
Jim Caley <jim.caley@ellucian.com>
2018-10-24 14:05:58 -0400

    Implement BXEGS-1322 "As a Proxy, the UI pages to display the Financial Aid Application Status must be responsive on desktop and portable devices" Complete Financial Aid Status refactoring to be located in finaid directory. Fix media query to properly identify lower end of desktop-only range.

--- web-app/proxyApp/finaid/proxyViewFinaidStatus-controller.js
+++ web-app/proxyApp/finaid/proxyViewFinaidStatus-controller.js
@@ -4,13 +4,33 @@
 proxyAppControllers.controller('proxyViewFinaidStatusController',['$scope','$rootScope','$stateParams', 'proxyAppService', '$filter',
     function ($scope, $rootScope, $stateParams, proxyAppService, $filter) {
 
-        $scope.financialAidStatus = {};
+        var financialAidStatusAsArray = function(finaidStatus) {
+            var retArr = [];
+
+            // Push values into array in a specific order
+            if (finaidStatus.hasOwnProperty('costOfAttendance') && finaidStatus.costOfAttendance) {
+                retArr.push(finaidStatus.costOfAttendance);
+            }
+
+            if (finaidStatus.hasOwnProperty('awardPackage') && finaidStatus.awardPackage) {
+                retArr.push(finaidStatus.awardPackage);
+            }
+
+            if (finaidStatus.hasOwnProperty('accountSummary') && finaidStatus.accountSummary) {
+                retArr.push(finaidStatus.accountSummary);
+            }
+
+            if (finaidStatus.hasOwnProperty('financialAidHistory') && finaidStatus.financialAidHistory) {
+                retArr.push(finaidStatus.financialAidHistory);
+            }
+
+            return retArr;
+        };
+
+        $scope.financialAidStatus = [];
         $scope.aidYearHolder = {
             aidYear: {}
         };
-        if(proxyAppService.getAidYear()) {
-            $scope.aidYearHolder.aidYear = proxyAppService.getAidYear();
-        }
 
         var init = function() {
 
@@ -21,14 +41,18 @@ proxyAppControllers.controller('proxyViewFinaidStatusController',['$scope','$roo
                 proxyAppService.setAidYear($scope.aidYearHolder.aidYear);
                 if(event.target.value != 'not/app') { // don't run query on "Not Applicable" selection
                     proxyAppService.getFinancialAidStatus({aidYear: event.target.value, pidm: $scope.pidm}).$promise.then(function (response) {
-                        $scope.financialAidStatus = response;
+                        $scope.financialAidStatus = financialAidStatusAsArray(response);
                     });
                 }
             });
 
+            if(proxyAppService.getAidYear()) {
+                $scope.aidYearHolder.aidYear = proxyAppService.getAidYear();
+            }
+
             if($scope.aidYearHolder.aidYear.code) {
                 proxyAppService.getFinancialAidStatus({aidYear: $scope.aidYearHolder.aidYear.code, pidm: sessionStorage.getItem("pidm")}).$promise.then(function (response) {
-                    $scope.financialAidStatus = response;
+                    $scope.financialAidStatus = financialAidStatusAsArray(response);;
                 });
             }
         };
