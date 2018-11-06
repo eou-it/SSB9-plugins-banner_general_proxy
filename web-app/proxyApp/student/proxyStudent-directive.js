a95056127725cdfb6c8cae48a14600318218bc99
Daren_Dunn <daren.dunn@ellucian.com>
2018-11-06 13:04:29 -0500

    BXEGS-1451 fix up mobile calendar buttons

--- web-app/proxyApp/student/proxyStudent-directive.js
+++ web-app/proxyApp/student/proxyStudent-directive.js
@@ -22,7 +22,33 @@ proxyAppDirectives.directive('fullCalendar',['proxyAppService', '$filter', '$com
 
     var weekOfText = $filter('i18n')('proxy.schedule.weekOf'),
         goToText = $filter('i18n')('proxy.schedule.goTo'),
-        datePlaceholderText = $filter('i18n')('default.date.format.watermark');
+        datePlaceholderText = $filter('i18n')('default.date.format.watermark'),
+        mobileOptions = {
+            defaultView: 'agendaTwoDay',
+            aspectRatio: 0.8,
+            header: {
+                left: 'title',
+                center: '',
+                right: ''
+            },
+            footer: {
+                right: '',
+                center: 'prevWeek prev next nextWeek'
+            }
+        },
+        desktopOptions = {
+            defaultView: 'agendaSevenDay',
+            aspectRatio: 2,
+            header: {
+                left: '',
+                center: 'title',
+                right: ''
+            },
+            footer: {
+                center: '',
+                right: 'prev next'
+            }
+        };
 
     return {
         link: function(scope, elem, attrs) {
@@ -32,45 +58,19 @@ proxyAppDirectives.directive('fullCalendar',['proxyAppService', '$filter', '$com
             //        return d.getHours() < 12 ? amPm[0] : amPm[1];
             //    };
 
-            var mobileOptions = {
-                    defaultView: 'agendaTwoDay',
-                    aspectRatio: 0.8,
-                    header: {
-                        left: 'title',
-                        center: 'myCustomButton2',
-                        right: ''
-                    },
-                    footer: {
-                        right: '',
-                        center: 'prevWeek prev next nextWeek'
-                    }
-                },
-                desktopOptions = {
-                    defaultView: 'agendaSevenDay',
-                    aspectRatio: 2,
-                    header: {
-                        left: '',
-                        center: 'title',
-                        right: ''
-                    },
-                    footer: {
-                        center: '',
-                        right: 'prev next'
-                    }
-                },
-                isMobile = $(window).width() < 768;
+            var isMobile = $(window).width() < 768;
 
                 $('#calendar').fullCalendar({
                     customButtons: {
                         nextWeek: {
-                            text: 'forward!',
+                            text: '>>',
                             click: function() {
                                 var tgtDate = $('#calendar').fullCalendar('getDate').add(7, 'days');
                                 $('#calendar').fullCalendar('gotoDate', tgtDate);
                             }
                         },
                         prevWeek: {
-                            text: 'backward!',
+                            text: '<<',
                             click: function() {
                                 var tgtDate = $('#calendar').fullCalendar('getDate').subtract(7, 'days');
                                 $('#calendar').fullCalendar('gotoDate', tgtDate);
@@ -169,7 +169,7 @@ proxyAppDirectives.directive('fullCalendar',['proxyAppService', '$filter', '$com
                         scrollToFirstEvent();
 
                     },
-                    eventAfterAllRender: function (v) {
+                    eventAfterAllRender: function (view) {
                         $("#calendar table.fc-agenda-days ").attr("dir", $.i18n.prop('default.language.direction') == 'ltr' ? "ltr" : "ltr");
                         if ($.i18n.prop('default.language.direction') == 'rtl') {
                             $(".fc-agenda-gutter.fc-widget-header.fc-last").removeClass("fc-last").addClass("fc-fake-last");
@@ -179,7 +179,34 @@ proxyAppDirectives.directive('fullCalendar',['proxyAppService', '$filter', '$com
                         }
 
                         var prevBtnElem = $('.fc-prev-button'),
-                            nextBtnElem = $('.fc-next-button');
+                            nextBtnElem = $('.fc-next-button'),
+                            backwardElem = null,
+                            forwardElem = null;
+
+                        if(view.name === 'agendaTwoDay')  {
+                            backwardElem = $('.fc-prevWeek-button');
+                            forwardElem = $('.fc-nextWeek-button');
+                            if(scope.hasPrevWeek) {
+                                backwardElem.removeClass('fc-state-disabled');
+                                backwardElem.removeAttr('disabled');
+                            }
+                            else {
+                                backwardElem.addClass('fc-state-disabled');
+                                backwardElem.attr('disabled', '');
+                            }
+                            backwardElem.addClass('secondary');
+
+                            if(scope.hasNextWeek) {
+                                forwardElem.removeClass('fc-state-disabled');
+                                forwardElem.removeAttr('disabled');
+                            }
+                            else {
+                                forwardElem.addClass('fc-state-disabled');
+                                forwardElem.attr('disabled', '');
+                            }
+                            forwardElem.addClass('secondary');
+                        }
+
                         if(scope.hasPrevWeek) {
                             prevBtnElem.removeClass('fc-state-disabled');
                             prevBtnElem.removeAttr('disabled');
@@ -202,13 +229,14 @@ proxyAppDirectives.directive('fullCalendar',['proxyAppService', '$filter', '$com
                     },
                     windowResize: function(view) {
                         var datePickerTemplate = null,
-                            datePickerElem = null;
+                            datePickerElem = null,
+                            fc = $('#calendar');
                         if ($(window).width() < 768) {
                             if(view.name === 'agendaSevenDay') {
-                                $('#calendar').fullCalendar('changeView', 'agendaTwoDay');
-                                $('#calendar').fullCalendar('option', 'aspectRatio', mobileOptions.aspectRatio);
-                                $('#calendar').fullCalendar('option', 'header', mobileOptions.header);
-                                $('#calendar').fullCalendar('option', 'footer', mobileOptions.footer);
+                                fc.fullCalendar('changeView', 'agendaTwoDay');
+                                fc.fullCalendar('option', 'aspectRatio', mobileOptions.aspectRatio);
+                                fc.fullCalendar('option', 'header', mobileOptions.header);
+                                fc.fullCalendar('option', 'footer', mobileOptions.footer);
 
                                 datePickerTemplate = '<div class="gotodate-block"> <label>'+ goToText +'</label> <input date-picker ng-model="tgtDate" pi-input-watcher on-select="goToDate" class="eds-text-field pi-date-input input-colors" placeholder="'+ datePlaceholderText +'" id="goToDate"/> </div>';
                                 datePickerElem = $compile(datePickerTemplate)(scope);
@@ -217,10 +245,10 @@ proxyAppDirectives.directive('fullCalendar',['proxyAppService', '$filter', '$com
                         }
                         else {
                             if(view.name === 'agendaTwoDay') {
-                                $('#calendar').fullCalendar('changeView', 'agendaSevenDay');
-                                $('#calendar').fullCalendar('option', 'aspectRatio', desktopOptions.aspectRatio);
-                                $('#calendar').fullCalendar('option', 'header', desktopOptions.header);
-                                $('#calendar').fullCalendar('option', 'footer', desktopOptions.footer);
+                                fc.fullCalendar('changeView', 'agendaSevenDay');
+                                fc.fullCalendar('option', 'aspectRatio', desktopOptions.aspectRatio);
+                                fc.fullCalendar('option', 'header', desktopOptions.header);
+                                fc.fullCalendar('option', 'footer', desktopOptions.footer);
 
                                 datePickerTemplate = '<div class="gotodate-block"> <label>' + goToText + '</label> <input date-picker ng-model="tgtDate" pi-input-watcher on-select="goToDate" class="eds-text-field pi-date-input input-colors" placeholder="' + datePlaceholderText + '" id="goToDate"/> </div>';
                                 datePickerElem = $compile(datePickerTemplate)(scope);
