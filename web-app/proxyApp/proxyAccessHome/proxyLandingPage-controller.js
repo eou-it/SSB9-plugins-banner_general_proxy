156cfc1e158a66755b8b52a5d5b400162b9a5065
Michael Hitrik <michael.hitrik@ellucian.com>
2019-01-09 14:46:12 -0500

    BXEGS-1546 - disabled Language Preference

--- web-app/proxyApp/proxyAccessHome/proxyLandingPage-controller.js
+++ web-app/proxyApp/proxyAccessHome/proxyLandingPage-controller.js
@@ -42,7 +42,7 @@ proxyAppControllers.controller('proxyLandingPageController',['$scope', '$rootSco
             $('#menu').removeClass('show').addClass('hide');
             $('#bannerMenu').removeClass('show').addClass('hide');
             //disable tools button
-            $('#toolsButton').removeClass('show').addClass('hide');
+            $('#Preference').removeClass('show').addClass('hide');
             $('#branding').removeAttr('href');
 
             proxyAppService.getStudentListForProxy().$promise.then(function (response) {
