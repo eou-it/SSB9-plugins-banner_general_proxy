03ee2c44415b6eeca8e9c7ea967f57b74a4f118e
Daren_Dunn <daren.dunn@ellucian.com>
2019-02-11 13:09:16 -0500

    BXEGS-1719 fix proxy service tests

--- test/integration/net/hedtech/banner/general/proxy/GeneralSsbProxyServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/general/proxy/GeneralSsbProxyServiceIntegrationTests.groovy
@@ -10,6 +10,7 @@ import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
 import net.hedtech.banner.general.person.PersonUtility
+import net.hedtech.banner.security.FormContext
 
 class GeneralSsbProxyServiceIntegrationTests extends BaseIntegrationTestCase {
 
@@ -19,18 +20,27 @@ class GeneralSsbProxyServiceIntegrationTests extends BaseIntegrationTestCase {
 
     @Before
     public void setUp() {
-        formContext = ['GUAGMNU']
-        super.setUp()
+        formContext = ['SELFSERVICE']
 
-        createProxy_0()
-        createProxy_1()
+        if (formContext) {
+            FormContext.set( formContext )
+        }
+
+        if (useTransactions) {
+            sessionFactory.currentSession.with {
+                connection().rollback()                 // needed to protect from other tests
+                clear()                                 // needed to protect from other tests
+                disconnect()                            // needed to release the old database connection
+                reconnect( dataSource.getConnection() ) // get a new connection that has unlocked the needed roles
+            }
+            transactionManager.getTransaction().setRollbackOnly()                 // and make sure we don't commit to the database
+            sessionFactory?.queryCache?.clear()                                     //clear the query cache when ehcache is being used
+        }
+        // super.setup() not called to prevent logging in as admin user which does not have necessary permissions
     }
 
     @After
     public void tearDown() {
-        deleteProxy_0()
-        deleteProxy_1()
-        deleteProxyPageWithRole()
         super.tearDown()
     }
 
@@ -45,20 +55,19 @@ class GeneralSsbProxyServiceIntegrationTests extends BaseIntegrationTestCase {
 
     @Test
     void testStudentList() {
-
-        def result = generalSsbProxyService.getStudentListForProxy(-1)
+        def gidm = getProxyIdm('mrbunny@gvalleyu.edu')
+        def result = generalSsbProxyService.getStudentListForProxy(gidm)
 
         assertNotNull result.students
-        assertEquals PersonUtility.getPerson("GDP000005").pidm, result.students[0].pidm
-        assertEquals PersonUtility.getPerson("HOS00001").pidm, result.students[1].pidm
+        assertTrue result.students.active.id.contains('HOSP0002')
+        assertTrue result.students.active.id.contains('HOS00001')
     }
 
 
     @Test
     void testStudentListNoAccess() {
-        deleteProxy_0()
-        createProxyNoAccess()
         def result = generalSsbProxyService.getStudentListForProxy(-1)
+
         assertTrue result?.students?.active.size() == 0
     }
 
@@ -66,12 +75,13 @@ class GeneralSsbProxyServiceIntegrationTests extends BaseIntegrationTestCase {
     void testSetProxyExpiredActionLink() {
         // expired letter SSS_REGD_USER2	SS_PINRESET2    AAAgFJAAFAAAspFAAB
         // set encodedRowId to that letter's row id encoded as base64
-        String encodedRowId = 'QUFBZ0ZKQUFGQUFBc3BGQUFC'
+        def gidm = getProxyIdm('sss01@ssb.com')
+        String encodedRowId = getExpiredLetterEncodedRowId(gidm)
         def result = generalSsbProxyService.setProxy(encodedRowId)
 
         assertNull result.gidm
         assertTrue result.login
-        assertEquals 'token-expire', result.message
+        assertEquals 'tokenExpired', result.message
         assertFalse result.verify
         assertFalse result.doPin
         assertFalse result.error
@@ -80,301 +90,41 @@ class GeneralSsbProxyServiceIntegrationTests extends BaseIntegrationTestCase {
 
     @Test
     void testGetProxyPages() {
-        addProxyPageWithRole()
-        updateProxyRETP()
-        def result = generalSsbProxyService.getProxyPages(-1, PersonUtility.getPerson("GDP000005").pidm )
+        def gidm = getProxyIdm('mrbunny@gvalleyu.edu')
+        def result = generalSsbProxyService.getProxyPages(gidm, PersonUtility.getPerson("HOSP0002").pidm )
 
-        def page = result.pages.find { it -> it.url.equals("/ssb/proxy/mypage") }
+        def page = result.pages.find { it -> it.url.equals("/ssb/proxy/courseScheduleDetail") }
 
         assertNotNull page
-        deleteProxyPageWithRole()
     }
 
 
-    def createProxy_0() {
-
-        def pidm = PersonUtility.getPerson("GDP000005").pidm
+    /*
+     * Could probably use net.hedtech.banner.general.events.ProxyAccessCredentialInformation,
+     * but don't want to add that dependency this late in the game
+     */
+    def getProxyIdm(String email) {
+        String gidm = '0'
+        String gpbprxySql = "select gpbprxy_proxy_idm as gidm from gpbprxy where gpbprxy_email_address = ?"
         def sql = new Sql(sessionFactory.getCurrentSession().connection())
-
-        sql.call("""
-declare
-
-   lv_proxyIDM    gpbprxy.gpbprxy_proxy_idm%TYPE := 0;
-   lv_pinhash     gpbprxy.gpbprxy_pin%TYPE;
-   lv_salt        gpbprxy.gpbprxy_salt%TYPE;
-   lv_hold_rowid  gb_common.internal_record_id_type;
-   lv_GPBPRXY_rec gp_gpbprxy.gpbprxy_rec;
-   lv_GPBPRXY_ref gp_gpbprxy.gpbprxy_ref;
-   lv_proxy_url   VARCHAR2(300);
-
-begin
-
-  gp_gpbprxy.P_Create (
-      p_proxy_idm        => -1,
-      p_email_address    => 'z',
-      p_last_name        => 'a',
-      p_first_name       => 'b',
-      p_proxy_pidm       => ${pidm},
-      p_pin              => null,
-      p_pin_disabled_ind => 'C',
-      p_salt             => null,
-      p_entity_cde       => null,
-      p_id               => null,
-      p_email_ver_date   => NULL,
-      p_pin_exp_date     => NULL,
-      p_create_user      => goksels.f_get_ssb_id_context,
-      p_create_date      => SYSDATE,
-      p_user_id          => goksels.f_get_ssb_id_context,
-      p_opt_out_adv_date => NULL,
-      p_rowid_out        => lv_hold_rowid
-      );
-
-
-        gp_gprxref.P_Create (
-         p_proxy_idm   => -1,
-         p_person_pidm => ${pidm},
-         p_retp_code   => 'AAA',
-         p_proxy_desc  => NULL,
-         p_start_date  => TRUNC(SYSDATE-1),
-         p_stop_date   => TRUNC(SYSDATE + 1),
-         p_create_user => goksels.f_get_ssb_id_context,
-         p_create_date => SYSDATE,
-         p_user_id     => goksels.f_get_ssb_id_context,
-         p_passphrase  => NULL,
-         p_rowid_out   => lv_hold_rowid
-         );
-
-insert into TWGRROLE (TWGRROLE_PIDM,TWGRROLE_ROLE,TWGRROLE_ACTIVITY_DATE) values(${pidm},'WTAILORPROXYMGMT',sysdate);
-insert into TWGRROLE (TWGRROLE_PIDM,TWGRROLE_ROLE,TWGRROLE_ACTIVITY_DATE) values(${pidm},'WTAILORPROXYACCESS',sysdate);
-
-end;
-            """)
-    }
-
-
-    def createProxy_1() {
-
-        def pidm = PersonUtility.getPerson("HOS00001").pidm
-        def sql = new Sql(sessionFactory.getCurrentSession().connection())
-
-        sql.call("""
-declare
-
-   lv_proxyIDM    gpbprxy.gpbprxy_proxy_idm%TYPE := 0;
-   lv_pinhash     gpbprxy.gpbprxy_pin%TYPE;
-   lv_salt        gpbprxy.gpbprxy_salt%TYPE;
-   lv_hold_rowid  gb_common.internal_record_id_type;
-   lv_GPBPRXY_rec gp_gpbprxy.gpbprxy_rec;
-   lv_GPBPRXY_ref gp_gpbprxy.gpbprxy_ref;
-   lv_proxy_url   VARCHAR2(300);
-
-begin
-
-
-        gp_gprxref.P_Create (
-         p_proxy_idm   => -1,
-         p_person_pidm => ${pidm},
-         p_retp_code   => 'AAA',
-         p_proxy_desc  => NULL,
-         p_start_date  => TRUNC(SYSDATE-1),
-         p_stop_date   => TRUNC(SYSDATE + 1),
-         p_create_user => goksels.f_get_ssb_id_context,
-         p_create_date => SYSDATE,
-         p_user_id     => goksels.f_get_ssb_id_context,
-         p_passphrase  => NULL,
-         p_rowid_out   => lv_hold_rowid
-         );
-
-insert into TWGRROLE (TWGRROLE_PIDM,TWGRROLE_ROLE,TWGRROLE_ACTIVITY_DATE) values(${pidm},'WTAILORPROXYMGMT',sysdate);
-insert into TWGRROLE (TWGRROLE_PIDM,TWGRROLE_ROLE,TWGRROLE_ACTIVITY_DATE) values(${pidm},'WTAILORPROXYACCESS',sysdate);
-
-end;
-            """)
-
-    }
-
-
-    def createProxyNoAccess() {
-
-        //p_start_date --> SYSDATE+10)
-
-        def pidm = PersonUtility.getPerson("GDP000005").pidm
-        def sql = new Sql(sessionFactory.getCurrentSession().connection())
-
-        sql.call("""
-declare
-
-   lv_proxyIDM    gpbprxy.gpbprxy_proxy_idm%TYPE := 0;
-   lv_pinhash     gpbprxy.gpbprxy_pin%TYPE;
-   lv_salt        gpbprxy.gpbprxy_salt%TYPE;
-   lv_hold_rowid  gb_common.internal_record_id_type;
-   lv_GPBPRXY_rec gp_gpbprxy.gpbprxy_rec;
-   lv_GPBPRXY_ref gp_gpbprxy.gpbprxy_ref;
-   lv_proxy_url   VARCHAR2(300);
-
-begin
-
-  gp_gpbprxy.P_Create (
-      p_proxy_idm        => -1,
-      p_email_address    => 'z',
-      p_last_name        => 'a',
-      p_first_name       => 'b',
-      p_proxy_pidm       => ${pidm},
-      p_pin              => null,
-      p_pin_disabled_ind => 'C',
-      p_salt             => null,
-      p_entity_cde       => null,
-      p_id               => null,
-      p_email_ver_date   => NULL,
-      p_pin_exp_date     => NULL,
-      p_create_user      => goksels.f_get_ssb_id_context,
-      p_create_date      => SYSDATE,
-      p_user_id          => goksels.f_get_ssb_id_context,
-      p_opt_out_adv_date => NULL,
-      p_rowid_out        => lv_hold_rowid
-      );
-
-
-        gp_gprxref.P_Create (
-         p_proxy_idm   => -1,
-         p_person_pidm => ${pidm},
-         p_retp_code   => 'AAA',
-         p_proxy_desc  => NULL,
-         p_start_date  => TRUNC(SYSDATE+10),
-         p_stop_date   => TRUNC(SYSDATE + 11),
-         p_create_user => goksels.f_get_ssb_id_context,
-         p_create_date => SYSDATE,
-         p_user_id     => goksels.f_get_ssb_id_context,
-         p_passphrase  => NULL,
-         p_rowid_out   => lv_hold_rowid
-         );
-
-insert into TWGRROLE (TWGRROLE_PIDM,TWGRROLE_ROLE,TWGRROLE_ACTIVITY_DATE) values(${pidm},'WTAILORPROXYMGMT',sysdate);
-insert into TWGRROLE (TWGRROLE_PIDM,TWGRROLE_ROLE,TWGRROLE_ACTIVITY_DATE) values(${pidm},'WTAILORPROXYACCESS',sysdate);
-
-end;
-            """)
-    }
-
-
-    def deleteProxy_0() {
-
-        def pidm = PersonUtility.getPerson("GDP000005").pidm
-
-        def sql = new Sql(sessionFactory.getCurrentSession().connection())
-
-        sql.call("""
-        begin
-        DELETE
-        FROM gprxref
-        WHERE gprxref_proxy_idm   = -1;
-
-        DELETE
-        FROM gpbprxy
-        WHERE gpbprxy_proxy_idm = -1;
-
-        DELETE FROM TWGRROLE WHERE TWGRROLE_PIDM = ${pidm};
-
-        commit;
-        end;
-            """)
-
-    }
-
-
-    def deleteProxy_1() {
-
-        def pidm = PersonUtility.getPerson("HOS00001").pidm
-
-        def sql = new Sql(sessionFactory.getCurrentSession().connection())
-
-        sql.call("""
-        begin
-        DELETE
-        FROM gprxref
-        WHERE gprxref_proxy_idm   = -1;
-
-        DELETE
-        FROM gpbprxy
-        WHERE gpbprxy_proxy_idm = -1;
-
-        DELETE FROM TWGRROLE WHERE TWGRROLE_PIDM = ${pidm};
-
-        commit;
-
-        end;
-            """)
-    }
-
-
-    // adds a new proxy page
-    private def addProxyPageWithRole() {
-        conn = dataSource.getConnection()
-
-        Sql sqlObj = new Sql( conn )
-        try {
-            sqlObj.executeInsert("INSERT INTO twgbwmnu (TWGBWMNU_NAME,TWGBWMNU_DESC,TWGBWMNU_PAGE_TITLE,TWGBWMNU_HEADER,TWGBWMNU_BACK_MENU_IND,TWGBWMNU_MODULE,TWGBWMNU_ENABLED_IND," +
-                    "TWGBWMNU_INSECURE_ALLOWED_IND,TWGBWMNU_ACTIVITY_DATE,TWGBWMNU_CACHE_OVERRIDE,TWGBWMNU_SOURCE_IND,TWGBWMNU_ADM_ACCESS_IND) VALUES " +
-                    "('/ssb/proxy/mypage','My Proxy Page','My Proxy Page','My Proxy Page','N','WTL','Y','N'," +
-                    "TO_TIMESTAMP('13-AUG-02','DD-MON-RR HH.MI.SSXFF AM'),'S','L','N')")
-
-            sqlObj.executeInsert("insert into TWGRMENU (TWGRMENU_NAME,TWGRMENU_SEQUENCE,TWGRMENU_URL_TEXT,TWGRMENU_URL,TWGRMENU_DB_LINK_IND,TWGRMENU_SUBMENU_IND,TWGRMENU_ACTIVITY_DATE,TWGRMENU_SOURCE_IND,TWGRMENU_ENABLED)\n" +
-                    "values ('PROXY_ACCESS_PARENT',99,'My Proxy Page','/ssb/proxy/mypage','Y','N',sysdate,'L','Y')")
-
-            sqlObj.executeInsert("insert into twgrwmrl (twgrwmrl_name, twgrwmrl_role,twgrwmrl_activity_date,twgrwmrl_source_ind)" +
-                    "values ('/ssb/proxy/mypage','PROXY_ACCESS_SUPPORT',TO_TIMESTAMP('13-AUG-02','DD-MON-RR HH.MI.SSXFF AM'),'L')")
-
-            sqlObj.commit();
-        } finally {
-            sqlObj?.close()
+        sql.eachRow(gpbprxySql, [email]) {trow ->
+            gidm = trow.gidm
         }
 
+        return gidm
     }
 
 
-    private def deleteProxyPageWithRole() {
-        conn = dataSource.getConnection()
-        Sql  sqlObj = new Sql( conn )
-        try {
-            sqlObj.execute("delete from  twgrmenu where TWGRMENU_SEQUENCE=99 and TWGRMENU_NAME = 'PROXY_ACCESS_PARENT'");
-            sqlObj.execute("delete from twgbwmnu where TWGBWMNU_NAME='/ssb/proxy/mypage' and TWGBWMNU_SOURCE_IND='L'");
-            sqlObj.execute("delete from twgrwmrl where twgrwmrl_name='/ssb/proxy/mypage' and twgrwmrl_SOURCE_IND='L'");
-            sqlObj.commit();
-        } finally {
-            sqlObj?.close()
-        }
-    }
-
-
-
-    def updateProxyRETP() {
-        def pidm = PersonUtility.getPerson("GDP000005").pidm
+    def getExpiredLetterEncodedRowId(String gidm) {
+        String rowId
+        String gpbeltrSql = """select twbkbssf.f_encode(rowid) as row_id from gpbeltr where GPBELTR_PROXY_IDM = ? 
+and GPBELTR_SYST_CODE = 'SSS_REGD_USER2' and gpbeltr_ctyp_code = 'SS_PINRESET2'"""
         def sql = new Sql(sessionFactory.getCurrentSession().connection())
+        sql.eachRow(gpbeltrSql, [gidm]) {trow ->
+            rowId = trow.row_id.toString()
+        }
 
-        sql.call("""
-declare
-
-   lv_proxyIDM    gpbprxy.gpbprxy_proxy_idm%TYPE := 0;
-   lv_pinhash     gpbprxy.gpbprxy_pin%TYPE;
-   lv_salt        gpbprxy.gpbprxy_salt%TYPE;
-   lv_hold_rowid  gb_common.internal_record_id_type;
-   lv_GPBPRXY_rec gp_gpbprxy.gpbprxy_rec;
-   lv_GPBPRXY_ref gp_gpbprxy.gpbprxy_ref;
-   lv_proxy_url   VARCHAR2(300);
-
-begin
-
-gp_gprxref.P_Update (
-      p_proxy_idm   => -1,
-      p_person_pidm => ${pidm},
-      p_retp_code   => 'PARENT',
-      p_start_date  => TRUNC(SYSDATE - 1),
-      p_stop_date   => TRUNC(SYSDATE + 10),
-      p_user_id     => goksels.f_get_ssb_id_context
-      );
-   gb_common.P_Commit;
-
-end;
-            """)
+        return rowId
     }
+
 }
