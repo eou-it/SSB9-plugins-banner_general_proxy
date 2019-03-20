87f18e008701fe97b0683fa92bd5c457e9574b09
Michael Hitrik <michael.hitrik@ellucian.com>
2019-03-20 11:52:25 -0400

    Remoced .gradle and build directory

--- build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '1.0')
-        classNode.putNodeMetaData('projectName', 'banner_general_proxy')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
