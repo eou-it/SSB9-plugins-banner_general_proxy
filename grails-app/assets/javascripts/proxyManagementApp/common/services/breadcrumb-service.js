b1b3544ed3e66373ef74e92f85f7019f9c7b237d
Jim Caley <jim.caley@ellucian.com>
2019-06-17 13:42:57 -0400

    Fix breadcrumb; add "Add New" control.

--- grails-app/assets/javascripts/proxyManagementApp/common/services/breadcrumb-service.js
+++ grails-app/assets/javascripts/proxyManagementApp/common/services/breadcrumb-service.js
@@ -1,17 +1,36 @@
 /*******************************************************************************
- Copyright 2016-2019 Ellucian Company L.P. and its affiliates.
+ Copyright 2019 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 
-proxyManagementApp.service( 'breadcrumbService', ['$filter',function ($filter) {
-    var constantBreadCrumb = [];
+proxyManagementApp.service( 'breadcrumbService', ['$filter', '$rootScope', function ($filter, $rootScope) {
+    var constantBreadCrumb = [],
+        callingUrl,
+        CALLING_URL = 1,
+        GEN_LANDING_PAGE_SIGNATURE;
+
+    $rootScope.applicationContextRoot = $('meta[name=applicationContextRoot]').attr("content");
+    GEN_LANDING_PAGE_SIGNATURE = new RegExp($rootScope.applicationContextRoot +'/ssb/general$');
 
     this.reset = function() {
+        var label;
+
         constantBreadCrumb = [
             {
-                label: 'proxy.landingpage.title',
-                url: '/'
+                label: 'general.breadcrumb.proxyManagement',
+                url: ''
             }
         ];
+
+        callingUrl = sessionStorage.getItem('genAppCallingPage');
+
+        if (callingUrl) {
+            label = GEN_LANDING_PAGE_SIGNATURE.test(callingUrl) ? 'banner.generalssb.landingpage.title' : 'default.paginate.prev';
+
+            constantBreadCrumb.splice(0, 0, {
+                label: label,
+                url: CALLING_URL
+            });
+        }
     };
 
     this.setBreadcrumbs = function (bc) {
@@ -20,17 +39,39 @@ proxyManagementApp.service( 'breadcrumbService', ['$filter',function ($filter) {
     };
 
     this.refreshBreadcrumbs = function() {
-        var updatedHeaderAttributes = {
-                "breadcrumb": {}
+        var breadCrumbInputData = {},
+            updatedHeaderAttributes,
+            backButtonUrl = '',
+            registerBackButtonClickListenerOverride = function(location) {
+                $('#breadcrumbBackButton').on('click',function(){
+                    window.location = location;
+                })
             };
 
         _.each (constantBreadCrumb, function(item) {
             var label = ($filter('i18n')(item.label));
 
-            updatedHeaderAttributes.breadcrumb[label] =
-                item.url ? ("/" + document.location.pathname.slice(Application.getApplicationPath().length+1) + "#"+item.url) : "";
+            if (item.url) {
+                if (item.url === CALLING_URL) {
+                    breadCrumbInputData[label] = callingUrl;
+                    backButtonUrl = callingUrl;
+                } else {
+                    breadCrumbInputData[label] = "/" + document.location.pathname.slice(Application.getApplicationPath().length + 1) + "#" + item.url;
+                }
+            } else {
+                breadCrumbInputData[label] = "";
+            }
         });
 
+        updatedHeaderAttributes = {
+            "breadcrumb":breadCrumbInputData
+        };
+
         BreadCrumbAndPageTitle.draw(updatedHeaderAttributes);
+
+        // As this is in a consolidated app, the default "previous breadcrumb" logic needs to be overridden to
+        // point the back button to the calling page URL.  (Note that the back button is only used for mobile,
+        // not desktop.)
+        registerBackButtonClickListenerOverride(backButtonUrl);
     };
 }]);
