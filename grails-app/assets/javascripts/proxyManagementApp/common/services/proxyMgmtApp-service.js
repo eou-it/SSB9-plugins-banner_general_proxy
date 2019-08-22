1c6847c0b658af652fed721c2c7647563a6a81ed
Jim Caley <jim.caley@ellucian.com>
2019-08-22 14:36:28 -0400

    Remove obsolete "add proxies list" code

--- grails-app/assets/javascripts/proxyManagementApp/common/services/proxyMgmtApp-service.js
+++ grails-app/assets/javascripts/proxyManagementApp/common/services/proxyMgmtApp-service.js
@@ -103,11 +103,6 @@ proxyManagementApp.service('proxyMgmtAppService', ['$rootScope', '$filter', '$re
             {controller: 'ProxyManagement', action: 'getDataModelOnAuthorizationChange'}).get(params);
     };
 
-    this.getAddProxiesList = function (params) {
-        return $resource('../ssb/:controller/:action',
-            {controller: 'ProxyManagement', action: 'getClonedProxyAddList'}).get(params);
-    };
-
     var sendCommunicationLog = function (params) {
         return $resource('../ssb/:controller/:action',
             {controller: 'ProxyManagement', action: 'sendCommunicationLog'}).get(params);
