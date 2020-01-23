3f5e532fe2faddacaa05bffc9884043f5be6da49
Jim Caley <jim.caley@ellucian.com>
2020-01-23 17:23:55 -0500

    Clean up unused Angular parameters.

--- grails-app/assets/javascripts/proxyApp/app.js
+++ grails-app/assets/javascripts/proxyApp/app.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright 2019 Ellucian Company L.P. and its affiliates.
+ Copyright 2019-2020 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 var proxyAppControllers = angular.module('proxyAppControllers', []);
 var proxyAppDirectives = angular.module('proxyAppDirectives', []);
