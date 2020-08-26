2cbcb3b85dd333b465e24c3a898e2f057c56dffa
Kyle Patrick <Kyle.Patrick@ellucian.com>
2020-08-26 09:16:25 -0400

    Added copyright to notification warning template and global proxy service

--- grails-app/services/net/hedtech/banner/general/proxy/GlobalProxyService.groovy
+++ grails-app/services/net/hedtech/banner/general/proxy/GlobalProxyService.groovy
@@ -1,3 +1,6 @@
+/********************************************************************************
+  Copyright 2020 Ellucian Company L.P. and its affiliates.
+********************************************************************************/
 package net.hedtech.banner.general.proxy
 
 import grails.gorm.transactions.Transactional
