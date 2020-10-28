0b3b8dd1efb983cb4d100da0c1dd8bc3142393a9
Kyle Patrick <Kyle.Patrick@ellucian.com>
2020-10-28 10:59:10 -0400

    BXEGS-2860 Implement Dirty Checking For Global Proxy & Correct Back Arrow Breadcrumb For Global Proxy Management

--- grails-app/assets/javascripts/globalProxyManagementApp/common/services/breadcrumb-service.js
+++ grails-app/assets/javascripts/globalProxyManagementApp/common/services/breadcrumb-service.js
@@ -3,25 +3,28 @@
  *******************************************************************************/
 
 globalProxyManagementApp.service( 'breadcrumbService', ['$filter', '$rootScope', function ($filter, $rootScope) {
-    var constantBreadCrumb = [],
+    let constantBreadCrumb = [],
         callingUrl,
+        globalProxyUrl,
         CALLING_URL = 1,
-        GEN_LANDING_PAGE_SIGNATURE;
+        GEN_LANDING_PAGE_SIGNATURE,
+        GLOBAL_PROXY_PAGE_URL;
 
     $rootScope.applicationContextRoot = $('meta[name=applicationContextRoot]').attr("content");
     GEN_LANDING_PAGE_SIGNATURE = new RegExp($rootScope.applicationContextRoot +'/ssb/general$');
+    GLOBAL_PROXY_PAGE_URL = $rootScope.applicationContextRoot  + '/ssb/globalProxy' + '#';
 
     this.reset = function() {
-        var label;
-
+        let label;
+        globalProxyUrl = sessionStorage.getItem('globalProxyInfoCallingPage');
         constantBreadCrumb = [
             {
                 label: 'general.breadcrumb.globalProxyManagement',
-                url: '/home'
+                url: globalProxyUrl ? GLOBAL_PROXY_PAGE_URL : ''
             }
         ];
 
-        callingUrl = sessionStorage.getItem('genAppCallingPage');
+        callingUrl = sessionStorage.getItem('genAppCallingPage') + '#';
 
         if (callingUrl) {
             label = GEN_LANDING_PAGE_SIGNATURE.test(callingUrl) ? 'banner.generalssb.landingpage.title' : 'default.paginate.prev';
@@ -36,20 +39,27 @@ globalProxyManagementApp.service( 'breadcrumbService', ['$filter', '$rootScope',
     this.setBreadcrumbs = function (bc) {
         this.reset();
         constantBreadCrumb.push.apply(constantBreadCrumb, bc);
+        if (bc[0]){
+            window.document.title = $filter('i18n')(bc[0].label);
+        }
     };
 
     this.refreshBreadcrumbs = function() {
-        var breadCrumbInputData = {},
+        let breadCrumbInputData = {},
             updatedHeaderAttributes,
             backButtonUrl = '',
             registerBackButtonClickListenerOverride = function(location) {
-                $('#breadcrumbBackButton').on('click',function(){
+                $('#breadcrumbBackButton').on('click keypress',function(){
                     window.location = location;
                 })
             };
 
+        let addTabIndexToBackButton = function(){
+            $('#breadcrumbBackButton').attr('tabindex','0');
+        };
+
         _.each (constantBreadCrumb, function(item) {
-            var label = ($filter('i18n')(item.label));
+            let label = ($filter('i18n')(item.label));
 
             if (item.url) {
                 if (item.url === CALLING_URL) {
@@ -57,6 +67,7 @@ globalProxyManagementApp.service( 'breadcrumbService', ['$filter', '$rootScope',
                     backButtonUrl = callingUrl;
                 } else {
                     breadCrumbInputData[label] = "/" + document.location.pathname.slice(Application.getApplicationPath().length + 1) + "#" + item.url;
+                    backButtonUrl = item.url;
                 }
             } else {
                 breadCrumbInputData[label] = "";
@@ -72,6 +83,7 @@ globalProxyManagementApp.service( 'breadcrumbService', ['$filter', '$rootScope',
         // As this is in a consolidated app, the default "previous breadcrumb" logic needs to be overridden to
         // point the back button to the calling page URL.  (Note that the back button is only used for mobile,
         // not desktop.)
+        addTabIndexToBackButton();
         registerBackButtonClickListenerOverride(backButtonUrl);
     };
 }]);
