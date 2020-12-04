2425fe6ee4d79f973314c8a24a7598e93ac24490
Kyle Patrick <Kyle.Patrick@ellucian.com>
2020-12-04 10:15:55 -0500

    BXEGS-3342 Add GP integration tests w/ new seeddata, fixed bugs found from tests

--- src/integration-test/groovy/net/hedtech/banner/general/proxy/GlobalProxyControllerIntegrationTests.groovy
+++ src/integration-test/groovy/net/hedtech/banner/general/proxy/GlobalProxyControllerIntegrationTests.groovy
@@ -7,6 +7,7 @@ import grails.util.GrailsWebMockUtil
 import grails.util.Holders
 import grails.web.servlet.context.GrailsWebApplicationContext
 import groovy.sql.Sql
+import net.hedtech.banner.i18n.MessageHelper
 import net.hedtech.banner.testing.BaseIntegrationTestCase
 import org.grails.plugins.testing.GrailsMockHttpServletRequest
 import org.grails.plugins.testing.GrailsMockHttpServletResponse
@@ -20,6 +21,7 @@ import org.junit.Test
 class GlobalProxyControllerIntegrationTests extends BaseIntegrationTestCase {
     def controller
     def sessionFactory
+    def sql
 
     /**
      * The setup method will run before all test case method executions start.
@@ -124,7 +126,7 @@ class GlobalProxyControllerIntegrationTests extends BaseIntegrationTestCase {
     @Test
     void testGetLoggedInUserHasActivePreferredEmail() {
         final def GLOBAL_PROXY_ID = 'HOPTE0606'
-        final def GLOBAL_PROXY_PIDM = 30205
+        final def GLOBAL_PROXY_PIDM = 30204
         def returnData
 
         mockRequest()
@@ -169,6 +171,134 @@ class GlobalProxyControllerIntegrationTests extends BaseIntegrationTestCase {
         }
     }
 
+    @Test
+    void testDeleteGlobalProxyTarget(){
+        disableEmailNotificationsForIntegrationTests()
+
+        final def GLOBAL_PROXY_ID = 'HOS00001'
+        final def EXISTING_RELATIONSHIP_ID = 'FASS20070'
+        final def INVALID_TOKEN = 'AAAAAAA'
+        final def NULL_TOKEN = ''
+        def studentToken
+        def existingProxies
+        def targetStudentFoundForGlobalProxy
+        def response
+
+        mockRequest()
+        SSBSetUp(GLOBAL_PROXY_ID, '111111')
+
+        //Make sure that the Global Proxy actually does have the existing relationship
+        controller.request.contentType = "text/json"
+        controller.getGlobalProxies()
+        assertThatDataExists(controller.response.contentAsString, false)
+        existingProxies = JSON.parse(controller.response.contentAsString)
+        targetStudentFoundForGlobalProxy = getExistingProxyFromExistingProxyList(existingProxies, EXISTING_RELATIONSHIP_ID)
+        assertThatDataExists(targetStudentFoundForGlobalProxy, false)
+        assertEquals(EXISTING_RELATIONSHIP_ID, targetStudentFoundForGlobalProxy.bannerId)
+        studentToken = targetStudentFoundForGlobalProxy.token
+
+        //Test Null Token
+        mockRequest()
+        def params = [token: NULL_TOKEN]
+        controller.params.putAll(params)
+        controller.deleteProxy()
+        assertThatDataExists(controller.response.contentAsString, false)
+        response = JSON.parse(controller.response.contentAsString)
+        assertTrue(response.failure)
+
+        //Test Invalid Token
+        mockRequest()
+        params = [token: INVALID_TOKEN]
+        controller.params.putAll(params)
+        controller.deleteProxy()
+        assertThatDataExists(controller.response.contentAsString, false)
+        response = JSON.parse(controller.response.contentAsString)
+        assertTrue(response.failure)
+
+        //Delete the existing relationship
+        mockRequest()
+        params = [token: studentToken]
+        controller.params.putAll(params)
+        controller.deleteProxy()
+        assertThatDataExists(controller.response.contentAsString, false)
+        existingProxies = JSON.parse(controller.response.contentAsString)
+        targetStudentFoundForGlobalProxy = getExistingProxyFromExistingProxyList(existingProxies, EXISTING_RELATIONSHIP_ID)
+        assertNull targetStudentFoundForGlobalProxy
+    }
+
+    @Test
+    void testCreateGlobalProxyRelationship(){
+        final def NON_EXISTENT_RELATIONSHIP_ID = 'HOSR24796'
+        final def INVALID_ID = 'AAAAAAAAA'
+        final def NON_STUDENT_ID = 'PBDDEV1'
+        final def INVALID_RELATIONSHIP = 'AAA'
+        final def NULL_ID = ''
+        final def GLOBAL_PROXY_ID = 'HOS00001'
+        def targetStudentFoundForGlobalProxy = null
+        def existingProxies
+        def response
+
+        mockRequest()
+        SSBSetUp(GLOBAL_PROXY_ID, '111111')
+        disableEmailNotificationsForIntegrationTests()
+
+        //Check to make sure that the non-existent relationship is truly non-existent
+        controller.request.contentType = "text/json"
+        controller.getGlobalProxies()
+        assertThatDataExists(controller.response.contentAsString, false)
+        existingProxies = JSON.parse(controller.response.contentAsString)
+        targetStudentFoundForGlobalProxy = getExistingProxyFromExistingProxyList(existingProxies, NON_EXISTENT_RELATIONSHIP_ID)
+        assertNull(targetStudentFoundForGlobalProxy)
+
+        //Test Invalid ID
+        mockRequest()
+        def params = [targetBannerId: INVALID_ID, retp: 'UNIV_FINAID']
+        controller.params.putAll(params)
+        controller.createGlobalProxyRelationship()
+        assertThatDataExists(controller.response.contentAsString, false)
+        response = JSON.parse(controller.response.contentAsString)
+        assertEquals( MessageHelper.message('globalProxyManagement.create.NOPERSON'), response.message)
+
+        //Test Null ID
+        mockRequest()
+        params = [targetBannerId: NULL_ID, retp: 'UNIV_FINAID']
+        controller.params.putAll(params)
+        controller.createGlobalProxyRelationship()
+        assertThatDataExists(controller.response.contentAsString, false)
+        response = JSON.parse(controller.response.contentAsString)
+        assertTrue(response.failure)
+
+        //Test Non-Proxiable Target
+        mockRequest()
+        params = [targetBannerId: NON_STUDENT_ID, retp: 'UNIV_FINAID']
+        controller.params.putAll(params)
+        controller.createGlobalProxyRelationship()
+        assertThatDataExists(controller.response.contentAsString, false)
+        response = JSON.parse(controller.response.contentAsString)
+        assertEquals( MessageHelper.message('globalProxyManagement.create.NOTARGET'), response.message)
+
+        //Test Invalid Relationship
+        mockRequest()
+        params = [targetBannerId: NON_EXISTENT_RELATIONSHIP_ID, retp: INVALID_RELATIONSHIP]
+        controller.params.putAll(params)
+        controller.createGlobalProxyRelationship()
+        assertThatDataExists(controller.response.contentAsString, false)
+        response = JSON.parse(controller.response.contentAsString)
+        assertTrue(response.failure)
+
+        //Add the relationship to the Global Proxy
+        mockRequest()
+        params = [targetBannerId: NON_EXISTENT_RELATIONSHIP_ID, retp: 'UNIV_FINAID']
+        controller.params.putAll(params)
+        controller.createGlobalProxyRelationship()
+        assertThatDataExists(controller.response.contentAsString, false)
+        existingProxies = JSON.parse(controller.response.contentAsString)
+        assertThatDataExists(existingProxies?.students?.active, true)
+        targetStudentFoundForGlobalProxy = getExistingProxyFromExistingProxyList(existingProxies, NON_EXISTENT_RELATIONSHIP_ID)
+        assertThatDataExists(targetStudentFoundForGlobalProxy, false)
+        assertEquals(NON_EXISTENT_RELATIONSHIP_ID, targetStudentFoundForGlobalProxy.bannerId)
+    }
+
     public GrailsWebRequest mockRequest() {
         GrailsMockHttpServletRequest mockRequest = new GrailsMockHttpServletRequest();
         GrailsMockHttpServletResponse mockResponse = new GrailsMockHttpServletResponse();
@@ -184,32 +314,42 @@ class GlobalProxyControllerIntegrationTests extends BaseIntegrationTestCase {
     }
 
     private def updateActiveIndicatorForGlobalProxyTargetRule(enable) {
+        def sql = new Sql(sessionFactory.getCurrentSession().connection())
         def activeInd = enable ? 'Y' : 'N'
-        def sqlConnection = getSqlConnection()
-
         //Sequence number two is delivered in seed data as rule which restricts global proxy targets to Student roles.
-        def sql = "UPDATE GORRSQL SET GORRSQL_ACTIVE_IND = ?, GORRSQL_VALIDATED_IND = ? WHERE GORRSQL_SQRU_CODE = 'SSB_ROLE_PROXYTARGET' AND GORRSQL_SEQ_NO = 2"
-        sqlConnection.executeUpdate(sql, [activeInd, activeInd])
-        sqlConnection.commit()
+        def sqlText = "UPDATE GORRSQL SET GORRSQL_ACTIVE_IND = ?, GORRSQL_VALIDATED_IND = ? WHERE GORRSQL_SQRU_CODE = 'SSB_ROLE_PROXYTARGET' AND GORRSQL_SEQ_NO = 2"
+        sql.executeUpdate(sqlText, [activeInd, activeInd])
+        sql.commit()
     }
 
     private def updateActivePreferredEmailAddress(active, preferred, pidm) {
+        def sql = new Sql(sessionFactory.getCurrentSession().connection())
         def activeInd = active ? 'A' : 'I'
         def preferredInd = preferred ? 'Y' : 'N'
-        def sqlConnection = getSqlConnection()
-        String sql = "UPDATE GOREMAL SET GOREMAL_STATUS_IND = ?, GOREMAL_PREFERRED_IND = ? WHERE GOREMAL_PIDM = ?"
-        sqlConnection.executeUpdate(sql, [activeInd, preferredInd, pidm])
-        sqlConnection.commit()
+        sql.executeUpdate("""UPDATE GOREMAL SET GOREMAL_STATUS_IND = ?, GOREMAL_PREFERRED_IND = ? WHERE GOREMAL_PIDM = ?""", [activeInd, preferredInd, pidm])
+        sql.commit()
     }
 
     private def updateProxyAccessPreferredNameSetting(value) {
-        def sqlConnection = getSqlConnection()
-        String sql = "UPDATE GURNHIR SET GURNHIR_USAGE = ? WHERE GURNHIR_PRODUCT = 'Banner Proxy' AND GURNHIR_APPLICATION = 'ProxyAccess'"
-        sqlConnection.executeUpdate(sql, [value])
-        sqlConnection.commit()
+        def sql = new Sql(sessionFactory.getCurrentSession().connection())
+        sql.executeUpdate("""UPDATE GURNHIR SET GURNHIR_USAGE = ? WHERE GURNHIR_PRODUCT = 'Banner Proxy' AND GURNHIR_APPLICATION = 'ProxyAccess'""", [value])
+        sql.commit()
     }
 
-    private def getSqlConnection() {
-        return new Sql(sessionFactory.getCurrentSession().connection())
+    private def disableEmailNotificationsForIntegrationTests(){
+        def sql = new Sql(sessionFactory.getCurrentSession().connection())
+        sql.executeUpdate("""UPDATE GEBCOMM SET GEBCOMM_PER_NOTIFY_IND = 'N', GEBCOMM_ADMIN_NOTIFY_IND = 'N', GEBCOMM_PROXY_NOTIFY_IND = 'N'
+        WHERE GEBCOMM_SYST_CODE= 'PROXY_GLOBAL_ACCESS'""")
+        sql.commit()
+    }
+
+    private def getExistingProxyFromExistingProxyList(existingProxyList, existingProxyBannerId){
+        def targetStudentFoundForGlobalProxy = null
+        existingProxyList?.students?.active?.each({activeStudent ->
+            if (activeStudent?.bannerId == existingProxyBannerId){
+                targetStudentFoundForGlobalProxy = activeStudent
+            }
+        })
+        return targetStudentFoundForGlobalProxy
     }
 }
